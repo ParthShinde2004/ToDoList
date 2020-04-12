98849484cc3c321e2a483b2809a6f468b9305369
NDseah <ndseah95@gmail.com>
2020-04-12 15:59:06 +0800

    Added task model

--- list/models.py
+++ list/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+	title = models.CharField(max_length=100)
+	due_date = models.DateTimeField(blank=True, default=None)
+	done = models.BooleanField(default=False)
+	tag = models.CharField(max_length=100, default="untagged")
+
