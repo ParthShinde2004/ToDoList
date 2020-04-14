d34082552874ac7c695319a38fffb4b4c42a00f9
NDseah <ndseah95@gmail.com>
2020-04-14 23:01:13 +0800

    Added dummy views and templates

--- list/views.py
+++ list/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Task
+
 # Create your views here.
 def home(request):
-	return HttpResponse("<p>home view</p>")
+	tasks = Task.objects.all()
+	return render(request, "home.html", {"tasks":tasks})
