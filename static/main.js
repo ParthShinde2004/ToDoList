ae9889c76b2ec8c29e1c6a28848314b760724630
NDseah <ndseah95@gmail.com>
2020-04-17 23:04:39 +0800

    Fixed some css design

--- static/main.js
+++ static/main.js
@@ -3,20 +3,30 @@ var categoryContent;
 
 window.onload = function () {
 	// categoryTabs = document.getElementById("sidebar").getElementsByTagName("li");
-	categoryTabs = document.getElementsByClassName("side");
+	categoryTabs = document.getElementsByClassName("side"); // categoryTabs has one more element than categoryContent
 	categoryContent = document.getElementById("categoryContent").getElementsByTagName("div");
 }
 
 function openCategory(category) {
 	// console.log(category)
 	// console.log(categoryTabs.length)
-	for (var i = 0; i < categoryTabs.length; i++ ) {
-		if (categoryTabs[i].id == 'tab'.concat(category)) {
-			categoryContent[i].style.display = "block"
-			console.log("matched: " + category)
+	if (category === "All") { 
+		categoryTabs[0].classList.add("active");
+	} else {
+		categoryTabs[0].classList.remove("active");
+	}
+	for (var i = 0; i < categoryContent.length; i++ ) {
+		if (category === "All") {
+			categoryTabs[i+1].classList.remove("active");
+			categoryContent[i].style.display = "block";
+		} else if (categoryTabs[i+1].id === 'tab'.concat(category)) {
+			categoryTabs[i+1].classList.add("active");
+			categoryContent[i].style.display = "block";
+			console.log("matched: " + category);
 		} else {
-			categoryContent[i].style.display = "none"
-			console.log("not matched: " + category)
+			categoryTabs[i+1].classList.remove("active");
+			categoryContent[i].style.display = "none";
+			console.log("not matched: " + category);
 		}
 	}
 }
